105334b5757cd370e3d4cec82c39134ee3878e06
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2025-11-22 18:47:34 -0300

    defining the database schema

---
+++ db_schema.sql
@@ -0,0 +1,13 @@
+﻿CREATE DATABASE IF NOT EXISTS stock_control;
+USE stock_control;
+
+CREATE TABLE IF NOT EXISTS products (
+    id_product INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    price DECIMAL(18, 2) NOT NULL,
+    flavor VARCHAR(100),
+    mark VARCHAR(100),
+    manufacturer VARCHAR(100),
+    due_date DATE,
+    quantity_stock INT NOT NULL
+);
